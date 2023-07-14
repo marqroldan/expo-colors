73998afd58d12bfc3f98c8d85a234dbd8c096a05
Marq Roldan <hcmlroldan@gmail.com>
2023-07-15 06:34:58 +0800

    Added personal color categories

---
+++ src/colors.ts
@@ -0,0 +1,50 @@
+export const colorCategories = [
+    {
+        redPink: [5.14,357.54]
+    },
+    {
+        red: [27.92]
+    },
+    {
+        orangeRed: [36.84]
+    },
+    {
+        orange: [36.84]
+    },
+    {
+        yellowOrange: [80.53]
+    },
+    {
+        yellow: [106.68]
+    },
+    {
+        greenYellow: [126.29]
+    },
+    {
+        green: [142.45]
+    },
+    {
+        cyanGreen: [165.211]
+    },
+    {
+        cyan: [187.23]
+    },
+    {
+        blueCyan: [222.36]
+    },
+    {
+        blue: [258.11]
+    },
+    {
+        violetBlue: [276.7]
+    },
+    {
+        violet: [290.98]
+    },
+    {
+        pinkViolet: [318.42]
+    },
+    {
+        pink: [341.67]
+    },
+]
